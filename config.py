fbfcfaeb224c1873b1477c278ea48e8229898f83
bernhard <bernhard.pfann@rbinternational.com>
2023-10-25 16:35:28 +0200

    changed parameters for word level tokens

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 # model hyperparameters
-block_size = 16 # 128
-embed_size = 32 # 128
-dropout = 0.2
+block_size = 32 # 128
+embed_size = 128 # 128
+dropout = 0.3
 n_heads = 6
 n_layer = 6
 eval_iters = 200
-batch_size = 32 
+batch_size = 64
 
 # learning hyperparameters
 learn_rate = 3e-4
-max_iters = 1000
+max_iters = 1_000
 eval_interval = 500
 
 # encoding
